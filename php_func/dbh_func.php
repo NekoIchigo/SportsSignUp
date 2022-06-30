4c8d4f89536be67e3602bf45724346657bad3518
NekoIchigo <reydanjohnbelen@gmail.com>
2022-07-01 03:46:31 +0800

    webhost db

--- php_func/dbh_func.php
+++ php_func/dbh_func.php
@@ -9,3 +9,7 @@ $conn = mysqli_connect($serverName, $dbUserName, $dbPassword, $dbName);
 if(!$conn){
   die("Connection failed" . mysqli_connect_error());
 }
+// 000webhost.com
+//db name = id19184961_sports_event_managemet
+//username = id19184961_sportsmanagement
+// pwd = 4LXoBW64(y2$j=-$
